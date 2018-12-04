4599eebcb7f44fd4bf4f790030291adb2631382d
Jiayao Wang <wjywangjiayao@gmail.com>
2018-12-04 15:30:27 +0100

    first commit

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 04.12.2018 14:33:08
+ * Author : itarch-user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
